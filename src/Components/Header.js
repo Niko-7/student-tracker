4e60829a2c6dcb89f0414b7c8f1e2ebc0f4bf016
Richard Mutch <rsmutch@gmail.com>
2020-12-02 14:16:38 +0000

    Planned app layout. Added header, nav bar and background image

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Northcoders Student Tracker</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>Students</button>
+      <button>Blocks</button>
+    </nav>
+  );
+};
+
+export default Nav;
